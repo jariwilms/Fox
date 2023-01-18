09f6f6609af2d6d8a816090f34843aed04b6c04a
Jari Wilms <jariwilms@outlook.com>
2023-01-18 21:43:03 +0100

    Experimental vao

--- Helix/src/Helix/EXP/Buffer/VertexBuffer.hpp
+++ Helix/src/Helix/EXP/Buffer/VertexBuffer.hpp
@@ -19,10 +19,29 @@ namespace hlx::exp
     protected:
         Id m_id{};
     };
-    class BufferObject : public IBindable
+    template<typename T>
+    class Buffer : public IBindable
     {
     public:
-        virtual ~BufferObject() = default;
+        virtual ~Buffer() = default;
+
+        void copy(const std::span<T>& data)
+        {
+            const auto dataSize = data.size() * sizeof(T);
+
+            if (data.size() * sizeof(T) != m_size) throw std::range_error{ "Data size is not equal to buffer size!" };
+
+            copy_base(data.data());
+        }
+        void copy_range(unsigned int offset, const std::span<T>& data)
+        {
+            const auto dataSize = data.size() * sizeof(T);
+            const auto offsetSize = offset * sizeof(T);
+
+            if (dataSize + offsetSize > m_size) throw std::range_error{ "Data size is greater than buffer range!" };
+
+            copy_range(dataSize, offsetSize, data.data());
+        }
 
         size_t size() const
         {
@@ -30,80 +49,78 @@ namespace hlx::exp
         }
 
     protected:
-        BufferObject(size_t size)
-            : m_size{ size } {}
+        Buffer(unsigned int count)
+            : m_size{ count * sizeof(T) } {}
 
-        size_t m_size{};
-    };
-
-    template<typename T>
-    class VertexBuffer : public BufferObject
-    {
-    public:
-        virtual ~VertexBuffer() = default;
-
-        void copy(const std::span<T>& data)
-        {
-            const auto dataSize = data.size() * sizeof(T);
-
-            if (data.size() * sizeof(T) != m_size) throw std::range_error{ "Data size is greater than buffer size!" };
-
-            copy(data.data());
-        }
-        void copy_range(unsigned int offset, const std::span<T>& data)
-        {
-            const auto dataSize = data.size() * sizeof(T);
-            const auto offsetSize = offset * sizeof(T);
-
-            if (dataSize + offsetSize > m_size) throw std::range_error{ "Data size is greater than buffer size!" };
-
-            copy_range(dataSize, offsetSize, data.data());
-        }
-
-    protected:
         virtual void copy(const void* data) = 0;
         virtual void copy_range(size_t size, size_t offset, const void* data) = 0;
 
-        VertexBuffer(unsigned int count)
-            : BufferObject{ count * sizeof(T) }
-        {
-            std::cout << "vbuffer called\n";
-        }
+        size_t m_size{};
     };
     template<typename T>
-    class UniformBuffer : public virtual VertexBuffer<T>
+    class UniformBuffer : public virtual Buffer<T>
     {
     public:
         virtual ~UniformBuffer() = default;
+
+        void copy(const T& data)
+        {
+            Buffer<T>::copy(&data);
+        }
+        template<typename... T>
+        void copy_range(size_t offset, const std::tuple<T...>& data)
+        {
+            Buffer<T>::copy_range(sizeof(std::tuple<T...>), offset, &data);
+        }
 
         virtual void bind_base(unsigned int index) = 0;
         virtual void bind_range(unsigned int index, size_t size, size_t offset) = 0;
 
     protected:
         UniformBuffer(unsigned int count)
-            : VertexBuffer<T>{ count } {}
+            : Buffer<T>{ count } {}
+    };
+
+    class VertexArray : public IBindable
+    {
+    public:
+        virtual ~VertexArray() = default;
+
+        template<typename T>
+        void tie(const std::shared_ptr<Buffer<T>> vertices, const std::shared_ptr<VertexLayout> layout)
+        {
+            tie(vertices->id());
+        }
+        virtual void tie(const std::shared_ptr<IndexBuffer> indices) = 0;
+
+    protected:
+        VertexArray() = default;
+
+        virtual void tie(Id bufferId, const std::shared_ptr<VertexLayout> layout) = 0;
+
+        unsigned int m_attributes{};
     };
 
 #include "glad/glad.h"
 #include "glfw/glfw3.h"
 
     template<typename T>
-    class OpenGLVertexBuffer : public virtual VertexBuffer<T>
+    class OpenGLBuffer : public virtual Buffer<T>
     {
     public:
-        OpenGLVertexBuffer(GLenum target, unsigned int count) 
-            : VertexBuffer<T>{ count }, m_internalTarget{ target }
+        OpenGLBuffer(GLenum target, unsigned int count)
+            : Buffer<T>{ count }, m_internalTarget{ target }
         {
             glCreateBuffers(1, &m_id);
             glNamedBufferStorage(m_id, m_size, nullptr, GL_DYNAMIC_STORAGE_BIT);
         }
-        OpenGLVertexBuffer(GLenum target, const std::span<T>& data) 
-            : VertexBuffer<T>{ static_cast<unsigned int>(data.size()) }, m_internalTarget{ target }
+        OpenGLBuffer(GLenum target, const std::span<T>& data) 
+            : Buffer<T>{ static_cast<unsigned int>(data.size()) }, m_internalTarget{ target }
         {
             glCreateBuffers(1, &m_id);
             glNamedBufferStorage(m_id, m_size, data.data(), GL_DYNAMIC_STORAGE_BIT);
         }
-        virtual ~OpenGLVertexBuffer()
+        virtual ~OpenGLBuffer()
         {
             glDeleteBuffers(1, &m_id);
         }
@@ -123,7 +140,7 @@ namespace hlx::exp
 
     protected:
         using IBindable::m_id;
-        using BufferObject::m_size;
+        using Buffer<T>::m_size;
 
         void copy(const void* data) override
         {
@@ -134,28 +151,39 @@ namespace hlx::exp
             glNamedBufferSubData(m_id, offset, size, data);
         }
 
-        GLenum m_internalTarget{ GL_VERTEX_ARRAY };
+        GLenum m_internalTarget{};
     };
-    class OpenGLIndexBuffer : public OpenGLVertexBuffer<unsigned int>
+
+    template<typename T>
+    class OpenGLVertexBuffer : public virtual OpenGLBuffer<T>
+    {
+    public:
+        OpenGLVertexBuffer(unsigned int count)
+            : OpenGLBuffer<T>{ GL_ARRAY_BUFFER, count }, Buffer<T>{ count } {}
+        OpenGLVertexBuffer(const std::span<unsigned int>& data)
+            : OpenGLBuffer<T>{ GL_ARRAY_BUFFER, data }, Buffer<T>{ static_cast<unsigned int>(data.size()) } {}
+        ~OpenGLVertexBuffer() = default;
+
+    protected:
+        using OpenGLBuffer<T>::m_internalTarget;
+    };
+    class OpenGLIndexBuffer : public OpenGLBuffer<unsigned int>
     {
     public:
         OpenGLIndexBuffer(unsigned int count)
-            : OpenGLVertexBuffer{ GL_ELEMENT_ARRAY_BUFFER, count }, VertexBuffer{ count } {}
+            : OpenGLBuffer{ GL_ELEMENT_ARRAY_BUFFER, count }, Buffer{ count } {}
         OpenGLIndexBuffer(const std::span<unsigned int>& data)
-            : OpenGLVertexBuffer{ GL_ELEMENT_ARRAY_BUFFER, data }, VertexBuffer{ static_cast<unsigned int>(data.size()) } {}
+            : OpenGLBuffer{ GL_ELEMENT_ARRAY_BUFFER, data }, Buffer{ static_cast<unsigned int>(data.size()) } {}
         ~OpenGLIndexBuffer() = default;
-
-    protected:
-        using OpenGLVertexBuffer<unsigned int>::m_internalTarget;
     };
     template<typename T>
-    class OpenGLUniformBuffer : public OpenGLVertexBuffer<T>, public UniformBuffer<T>
+    class OpenGLUniformBuffer : public OpenGLBuffer<T>, public UniformBuffer<T>
     {
     public:
         OpenGLUniformBuffer(unsigned int count) 
-            : OpenGLVertexBuffer<T>{ GL_UNIFORM_BUFFER, count }, UniformBuffer<T>{ count }, VertexBuffer<T>{ count } {}
+            : OpenGLBuffer<T>{ GL_UNIFORM_BUFFER, count }, UniformBuffer<T>{ count }, Buffer<T>{ count } {}
         OpenGLUniformBuffer(const std::span<T>& data)
-            : OpenGLVertexBuffer<T>{ GL_UNIFORM_BUFFER, data }, UniformBuffer<T>{ static_cast<unsigned int>(data.size()) }, VertexBuffer<T>{ static_cast<unsigned int>(data.size()) } {}
+            : OpenGLBuffer<T>{ GL_UNIFORM_BUFFER, data }, UniformBuffer<T>{ static_cast<unsigned int>(data.size()) }, Buffer<T>{ static_cast<unsigned int>(data.size()) } {}
         ~OpenGLUniformBuffer() = default;
 
         void bind_base(unsigned int index) override
@@ -169,19 +197,75 @@ namespace hlx::exp
 
     protected:
         using IBindable::m_id;
-        using BufferObject::m_size;
-        using OpenGLVertexBuffer<T>::m_internalTarget;
+        using OpenGLBuffer<T>::m_internalTarget;
     };
+
+    class OpenGLVertexArray : public VertexArray
+    {
+    public:
+        OpenGLVertexArray()
+        {
+            glCreateVertexArrays(1, &m_id);
+        }
+
+        void bind() override
+        {
+            glBindVertexArray(m_id);
+        }
+        void unbind() override
+        {
+            glBindVertexArray(0);
+        }
+        bool is_bound() override
+        {
+            return false;
+        }
+
+        void tie(Id bufferId, const std::shared_ptr<VertexLayout> layout) override
+        {
+            static int bindingIndex{};
+
+            glVertexArrayVertexBuffer(m_id, bindingIndex, bufferId, 0, static_cast<GLsizei>(layout->stride()));
+
+            GLint offset{};
+            for (const auto& attribute : layout->attributes())
+            {
+                glEnableVertexArrayAttrib(m_id, m_attributes);
+                glVertexArrayAttribFormat(m_id, m_attributes, attribute.count, attribute.internalType, attribute.isNormalized, offset);
+                glVertexArrayAttribBinding(m_id, m_attributes, bindingIndex);
+
+                offset += static_cast<GLint>(attribute.count * attribute.tSize);
+                ++m_attributes;
+            }
+
+            ++bindingIndex;
+        }
+        void tie(const std::shared_ptr<IndexBuffer> indices) override
+        {
+            glVertexArrayElementBuffer(m_id, indices->id());
+        }
+    };
+
+
+
+
+    template<typename T>
+    using VertexBuffer = Buffer<T>;
+    using IndexBuffer = Buffer<unsigned int>;
 
     template<typename T>
     std::shared_ptr<VertexBuffer<T>> create_vbo(unsigned int count)
     {
-        return std::make_shared<OpenGLVertexBuffer<T>>(GL_ARRAY_BUFFER, count);
+        return std::make_shared<OpenGLVertexBuffer<T>>(count);
     }
-    using IndexBuffer = VertexBuffer<unsigned int>;
     std::shared_ptr<IndexBuffer> create_ibo(unsigned int count)
     {
         return std::make_shared<OpenGLIndexBuffer>(count);
+    }
+    template<typename T>
+    std::shared_ptr<UniformBuffer<T>> create_ubo(unsigned int count = 1)
+    {
+        return std::make_shared<OpenGLUniformBuffer<T>>(count);
     }
 
     void test()
@@ -192,10 +276,10 @@ namespace hlx::exp
             float b{};
         };
 
+        TestStruct ts;
+
         auto vbo = create_vbo<float>(12);
         auto ibo = create_ibo(12);
-        //auto ubo = create_ubo<float>(12);
-
-        OpenGLUniformBuffer<float> ub{12};
+        auto ubo = create_ubo<TestStruct>(12);
     }
 }
