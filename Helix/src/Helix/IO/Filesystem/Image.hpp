da6edf179b6e4de5e21004ef3cb64335ba7b76d8
Jari Wilms <jariwilms@outlook.com>
2023-09-23 19:36:55 +0200

    Image cleanup

--- Helix/src/Helix/IO/Filesystem/Image.hpp
+++ Helix/src/Helix/IO/Filesystem/Image.hpp
@@ -14,19 +14,13 @@ namespace hlx
 	struct Image : public File
 	{
 	public:
-		struct Properties
-		{
-			Vector2u dimensions{};
-			size_t size{};
-			unsigned int channels{};
-			const std::vector<byte> data{};
-		};
-
 		explicit Image(const std::filesystem::path& path)
 			: File{ path }
 		{
 			const auto fileData = File::read();
 			stbi_info_from_memory(fileData->data(), static_cast<int>(m_size), reinterpret_cast<int*>(&m_dimensions.x), reinterpret_cast<int*>(&m_dimensions.y), reinterpret_cast<int*>(&m_channels));
+
+			//stbi_info(path.string().c_str(), reinterpret_cast<int*>(&m_dimensions.x), reinterpret_cast<int*>(&m_dimensions.y), reinterpret_cast<int*>(&m_channels));
 		}
 		~Image() = default;
 
@@ -48,24 +42,6 @@ namespace hlx
 
 			return ptr;
 		}
-		Properties read_c(unsigned int channels, bool flip = false)
-		{
-            stbi_set_flip_vertically_on_load(flip);
-
-			Vector2u dimensions{};
-			size_t size{};
-
-            const auto& fileData = File::read();
-            const auto& imageData = stbi_load_from_memory(fileData->data(), static_cast<int>(fileData->size()), reinterpret_cast<int*>(&dimensions.x), reinterpret_cast<int*>(&dimensions.y), nullptr, channels);
-            size = (static_cast<size_t>(dimensions.x) * static_cast<size_t>(dimensions.y)) * channels;
-
-			const std::vector<byte> data{ imageData, imageData + size };
-			auto result = Properties{ dimensions, size, channels, data };
-
-            stbi_image_free(imageData);
-
-			return result;
-		}
 		void write(std::span<const byte>& data) override
 		{
 			throw std::logic_error{ "Not implemented!" };
@@ -86,7 +62,7 @@ namespace hlx
 		}
 
 	private:
-		Vector2u m_dimensions{};
+		Vector2u     m_dimensions{};
 		unsigned int m_channels{};
 
 		std::weak_ptr<const std::vector<byte>> m_imageData{};
