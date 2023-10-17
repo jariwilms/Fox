b7b65ec1432a62b7dd85bd24d8b7053d7dd8b7a7
Jari Wilms <jariwilms@outlook.com>
2023-10-18 01:45:54 +0200

    using primitive types

--- Helix/src/Helix/Core/Typedef.hpp
+++ Helix/src/Helix/Core/Typedef.hpp
@@ -2,10 +2,19 @@
 
 #include <glm/glm.hpp>
 
-using byte  = unsigned char;
-using word  = unsigned short;
-using dword = unsigned int;
-using qword = unsigned long long;
+using byte       = unsigned char;
+using word       = unsigned short;
+using dword      = unsigned int;
+using qword      = unsigned long long;
+
+using i8         = std::int8_t;
+using i16        = std::int16_t;
+using i32        = std::int32_t;
+using i64        = std::int64_t;
+using ui8        = std::uint8_t;
+using ui16       = std::uint16_t;
+using ui32       = std::uint32_t;
+using ui64_t     = std::uint64_t;
 
 using Vector2i   = glm::ivec2;
 using Vector3i   = glm::ivec3;
@@ -21,5 +30,5 @@ using Matrix3f   = glm::mat3;
 using Matrix4f   = glm::mat4;
 using Quaternion = glm::quat;
 
-using t_hash = size_t;
-using Id = unsigned int;
+using t_hash     = size_t;
+using Id         = unsigned int;
