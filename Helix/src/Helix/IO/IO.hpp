8c422c8378652974b6b21f004e078bee0c09ea7b
Jari Wilms <jariwilms@outlook.com>
2023-11-04 11:02:14 +0100

    IO changes

--- Helix/src/Helix/IO/IO.hpp
+++ Helix/src/Helix/IO/IO.hpp
@@ -3,55 +3,27 @@
 #include "stdafx.hpp"
 
 #include "Filesystem/File.hpp"
-#include "Filesystem/Image.hpp"
 #include "Filesystem/Directory.hpp"
 
-namespace hlx
+namespace hlx::io
 {
-	class IO
-	{
-	public:
-		static void init()
-		{
-
-		}
-		
         template<typename T>
-        static std::shared_ptr<T> load(const std::filesystem::path& path) = delete;
-        template<> static std::shared_ptr<File>  load(const std::filesystem::path& path)
+        concept PathConstructible = requires (T t)
         {
-            const auto cumulativePath = (s_root / path).lexically_normal();
+            T{ std::filesystem::path{} };
+        };
 
-            const auto& it = m_files.find(cumulativePath);
-            if (it != m_files.end() && !it->second.expired()) return it->second.lock();
+        const Directory& root()
+        {
+            static Directory rootDirectory{ ASSET_DIR };
 
-            const auto& file = std::make_shared<File>(cumulativePath);
-            m_files.emplace(cumulativePath, file);
-
-            return file;
+            return rootDirectory;
         }
-        //template<> static std::shared_ptr<Image> load(const std::filesystem::path& path)
-        //{
-        //    const auto cumulativePath = s_root / path;
-        //
-        //    const auto it = m_images.find(cumulativePath);
-        //    if (it != m_images.end() && !it->second.expired()) return it->second.lock();
-        //
-        //    const auto image = std::make_shared<Image>(cumulativePath);
-        //    m_images.emplace(cumulativePath, image);
-        //
-        //    return image;
-        //}
 
-		static Directory& root()
-		{
-			return s_root;
-		}
+        std::shared_ptr<File> load(const std::filesystem::path& path)
+        {
+            const auto filePath = (root().path() / path).lexically_normal();
 
-	private:
-		static inline Directory s_root{ ASSET_DIR };
-
-		static inline std::unordered_map<std::filesystem::path, std::weak_ptr<File>> m_files{};
-		static inline std::unordered_map<std::filesystem::path, std::weak_ptr<Image>> m_images{};
-	};
+            return std::make_shared<File>(filePath);
+        }
 }
--- Helix/src/Helix/Rendering/Utility/Utility.hpp
+++ Helix/src/Helix/Rendering/Utility/Utility.hpp
@@ -3,23 +3,24 @@
 #include "stdafx.hpp"
 
 #include "Helix/IO/Filesystem/File.hpp"
+#include "Helix/IO/IO.hpp"
 #include "Helix/Rendering/Rendering.hpp"
 
 namespace hlx::gfx
 {
-    //auto shaders_from_source()
-    //{
-    //    throw std::logic_error{ "The method or operation has not been implemented!" };
-    //}
+    auto shaders_from_source()
+    {
+        throw std::logic_error{ "The method or operation has not been implemented!" };
+    }
     auto shaders_from_binaries(std::string_view vertex, std::string_view fragment)
     {
-        auto vertexFile   = File{ vertex };
-        auto fragmentFile = File{ fragment };
+        const auto& vertexFile   = io::load(vertex);
+        const auto& fragmentFile = io::load(fragment);
 
         return std::array<std::shared_ptr<Shader>, 2>
         {
-            std::make_shared<Shader>(Shader::Stage::Vertex,   *vertexFile.read()), 
-            std::make_shared<Shader>(Shader::Stage::Fragment, *fragmentFile.read()), 
+            std::make_shared<Shader>(Shader::Stage::Vertex,   *vertexFile->read()), 
+            std::make_shared<Shader>(Shader::Stage::Fragment, *fragmentFile->read()), 
         };
     }
 }
