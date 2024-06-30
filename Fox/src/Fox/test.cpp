29f16f027b31fb81b02b3274334a54b15e6f5cef
Jari Wilms <jariwilms@outlook.com>
2024-06-30 10:46:20 +0200

    unincluded PCH fix

--- Fox/src/Fox/test.cpp
+++ Fox/src/Fox/test.cpp
@@ -1,20 +1,22 @@
-#include "Fox/Core/Using.hpp"
-#include "Fox/Rendering/Rendering.hpp"
-#include "Fox/Rendering/Renderer.hpp"
+#include "stdafx.hpp"
 
-static int test()
-{
-    using namespace fox::gfx;
-
-    RenderInfo* ri;
-
-    Renderer::init();
-    Renderer::start(*ri);
-    Renderer::finish();
-
-
-
-
-
-    return 0;
-}
+//#include "Fox/Core/Using.hpp"
+//#include "Fox/Rendering/Rendering.hpp"
+//#include "Fox/Rendering/Renderer.hpp"
+//
+//static int test()
+//{
+//    using namespace fox::gfx;
+//
+//    RenderInfo* ri;
+//
+//    Renderer::init();
+//    Renderer::start(*ri);
+//    Renderer::finish();
+//
+//
+//
+//
+//
+//    return 0;
+//}
