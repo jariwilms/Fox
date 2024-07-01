7fd0e8bb1d8ee4648bbef93e39219c9cdf849823
Jari Wilms <jariwilms@outlook.com>
2024-07-01 14:05:21 +0200

    Simple main setup

--- Fox/src/main.cpp
+++ Fox/src/main.cpp
@@ -4,6 +4,7 @@
 #include "Fox/Rendering/Rendering.hpp"
 #include "Fox/Rendering/Create.hpp"
 #include "Fox/Rendering/Renderer.hpp"
+#include "Fox/Window/WindowManager.hpp"
 
 #include "Fox/Rendering/RenderInfo/RenderInfo.hpp"
 
@@ -11,25 +12,17 @@ using namespace fox;
 
 int main(int argc, char* argv[])
 {
-    Camera camera;
-    Transform t{};
+    const auto& window = WindowManager::create("Window", "Fox", Vector2u{ 1280, 720 });
+
+
+    Camera camera; Transform t{};
     std::array<std::tuple<Light, Vector3f>, 32> lights{};
-
-
-
-    Application application{ argc, argv };
-
-
 
     gfx::RenderInfo ri{ std::make_tuple(camera, t), lights };
 
     gfx::Renderer::init();
     gfx::Renderer::start(ri);
     gfx::Renderer::finish();
-
-
-
-    application.run();
 
 
 
