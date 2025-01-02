61d0ecb9e2fe52c9cddea18e401f19f84b10bbca
Jari Wilms <jariwilms@outlook.com>
2025-01-02 21:14:43 +0100

    RenderBuffer template specialization split

--- Fox/src/Fox/Rendering/API/OpenGL/Texture/RenderBuffer.hpp
+++ Fox/src/Fox/Rendering/API/OpenGL/Texture/RenderBuffer.hpp
@@ -5,11 +5,14 @@
 
 namespace fox::gfx::api::gl
 {
-    template<AntiAliasing AA>
-    class RenderBuffer : public api::Texture, public gl::Object
+    template<AntiAliasing>
+    class RenderBuffer;
+
+    template<>
+    class RenderBuffer<AntiAliasing::None> : public api::Texture, public gl::Object
     {
     public:
-        RenderBuffer(Format format, const fox::Vector2u& dimensions) requires (AA == AntiAliasing::None)
+        RenderBuffer(Format format, const fox::Vector2u& dimensions)
             : api::Texture{ format }, m_dimensions{ dimensions }
         {
             m_handle = gl::create_render_buffer();
@@ -17,18 +20,7 @@ namespace fox::gfx::api::gl
             const auto& renderBufferFormat = gl::map_render_buffer_format(format);
             gl::render_buffer_storage(m_handle, renderBufferFormat, m_dimensions);
         }
-        RenderBuffer(Format format, const fox::Vector2u& dimensions, fox::count_t samples) requires (AA == AntiAliasing::MSAA)
-            : Texture{ format }, m_dimensions{ dimensions }, m_samples{ samples }
-        {
-            m_handle = gl::create_render_buffer();
-
-            const auto& renderBufferFormat = gl::map_render_buffer_format(format);
-            gl::render_buffer_storage_multisample(m_handle, renderBufferFormat, m_dimensions, m_samples);
-        }
-        RenderBuffer(RenderBuffer&& other) noexcept
-        {
-            *this = std::move(other);
-        }
+        RenderBuffer(RenderBuffer&& other) noexcept = default;
         ~RenderBuffer()
         {
             gl::delete_render_buffer(m_handle);
@@ -38,26 +30,43 @@ namespace fox::gfx::api::gl
         {
             return m_dimensions;
         }
-        fox::count_t         samples()    const
+
+        RenderBuffer& operator=(RenderBuffer&& other) noexcept = default;
+
+    private:
+        fox::Vector2u m_dimensions{};
+    };
+    template<>
+    class RenderBuffer<AntiAliasing::MSAA> : public api::Texture, public gl::Object
+    {
+    public:
+        RenderBuffer(Format format, const fox::Vector2u& dimensions, fox::uint8_t samples)
+            : api::Texture{ format }, m_dimensions{ dimensions }, m_samples{ samples }
+        {
+            m_handle = gl::create_render_buffer();
+
+            const auto& renderBufferFormat = gl::map_render_buffer_format(format);
+            gl::render_buffer_storage_multisample(m_handle, renderBufferFormat, m_dimensions, m_samples);
+        }
+        RenderBuffer(RenderBuffer&& other) noexcept = default;
+        ~RenderBuffer()
+        {
+            gl::delete_render_buffer(m_handle);
+        }
+
+        const fox::Vector2u& dimensions() const
+        {
+            return m_dimensions;
+        }
+        fox::uint8_t         samples() const
         {
             return m_samples;
         }
 
-        RenderBuffer& operator=(RenderBuffer&& other) noexcept
-        {
-            m_handle     = other.m_handle;
-            m_dimensions = other.m_dimensions;
-            m_samples    = other.m_samples;
-
-            other.m_handle     = {};
-            other.m_dimensions = {};
-            other.m_samples    = 0u;
-
-            return *this;
-        }
+        RenderBuffer& operator=(RenderBuffer&& other) noexcept = default;
 
     private:
         fox::Vector2u m_dimensions{};
-        fox::count_t  m_samples{};
+        fox::uint8_t  m_samples{};
     };
 }
