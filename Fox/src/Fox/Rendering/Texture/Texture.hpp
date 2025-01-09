1f9a5e14e3a8b4ab1e9173c4f14b765f7f2ef4b7
Jari Wilms <jariwilms@outlook.com>
2025-01-09 16:11:45 +0100

    Naming fix

--- Fox/src/Fox/Rendering/Texture/Texture.hpp
+++ Fox/src/Fox/Rendering/Texture/Texture.hpp
@@ -9,16 +9,12 @@ namespace fox::gfx::api
     public:
         enum class Format
         {
-            //UNORM   => stored as unsigned integer, sampled as   floating point in range [        0,          1]
-            //SNORM   => stored as   signed integer, sampled as   floating point in range [       -1,          1]
-            //USCALED => stored as   floating point, sampled as   floating point in range [        0,   UINT_MAX]
-            //SSCALED => stored as   floating point, sampled as   floating point in range [  INT_MIN,    INT_MAX]
-            //UINT    => stored as unsigned integer, sampled as unsigned integer in range [        0,   UINT_MAX]
-            //SINT    => stored as   signed integer, sampled as   signed integer in range [  INT_MIN,    INT_MAX]
-            //UFLOAT  => stored as   floating point, sampled as   floating point in range [        0, UFLOAT_MAX]
-            //SFLOAT  => stored as   floating point, sampled as   floating point in range [FLOAT_MIN,  FLOAT_MAX]
-            //SRGB    => stored as unsigned integer, sampled as   floating point in range [        0,   UINT_MAX]
-            //Note: The Red, Green and Blue components of SRGB formats are nonlinear
+            //UNORM   => stored as unsigned integer, sampled as   floating point in range [        0,         1]
+            //SNORM   => stored as   signed integer, sampled as   floating point in range [       -1,         1]
+            //SRGB    => stored as unsigned integer, sampled as   floating point in range [        0,  UINT_MAX]
+            //UINT    => stored as unsigned integer, sampled as unsigned integer in range [        0,  UINT_MAX]
+            //FLOAT   => stored as   floating point, sampled as   floating point in range [FLOAT_MIN, FLOAT_MAX]
+            //Note: The Red, Green and Blue components of sRGB formats are nonlinear
 
             //Format: 0x|Size Component 1|'|Size Component 2|'|No. Components|'|Primary Key|
 
