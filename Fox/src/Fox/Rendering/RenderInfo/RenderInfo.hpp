a1df729f7817ce3914f13951c807926712e98011
Jari Wilms <jariwilms@outlook.com>
2025-01-23 12:22:34 +0100

    formatting

--- Fox/src/Fox/Rendering/RenderInfo/RenderInfo.hpp
+++ Fox/src/Fox/Rendering/RenderInfo/RenderInfo.hpp
@@ -9,11 +9,11 @@
 
 namespace fox::gfx
 {
+    //A simple view class for a scene's camera, lights, and skybox
     struct RenderInfo
     {
-        std::tuple<const fox::Camera&, const fox::Transform&> camera;
+        std::tuple<const fox::Camera&, const fox::Transform&>  camera;
         std::span<const std::tuple<fox::Light, fox::Vector3f>> lights;
-
-        std::shared_ptr<const gfx::Cubemap> skybox;
+        std::shared_ptr<const gfx::Cubemap>                    skybox;
     };
 }
