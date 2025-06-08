bbfec24c1f5670ab7f1a009404da0e918f427fc1
Jari Wilms <jariwilms@outlook.com>
2025-06-08 16:16:45 +0200

    json library

---
+++ Fox/vendor/nlohmann/premake5.lua
@@ -0,0 +1,29 @@
+project "NLOHMANN"
+	language      "C++"
+	cppdialect    "C++17"
+	kind          "StaticLib"
+	staticruntime "On"
+	
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir    ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}/obj")
+	
+	files
+	{
+		"include/nlohmann/json.hpp", 
+	}
+	
+	includedirs
+	{
+		"include", 
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+		
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
