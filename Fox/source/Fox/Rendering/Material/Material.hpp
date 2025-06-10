9f9efbf15750fd61b95dc298685d7aef7d22b381
Jari Wilms <jariwilms@outlook.com>
2025-06-10 23:30:08 +0200

    formatting

--- Fox/source/Fox/Rendering/Material/Material.hpp
+++ Fox/source/Fox/Rendering/Material/Material.hpp
@@ -11,13 +11,13 @@ namespace fox::gfx
     public:
         std::string name{ "Material" };
 
-        fox::Vector4f  color{ 1.0f };
+        fox::Vector4f  color          { 1.0f };
         fox::float32_t roughnessFactor{ 0.5f };
-        fox::float32_t metallicFactor{ 0.0f };
+        fox::float32_t metallicFactor { 0.0f };
 
-        std::shared_ptr<const gfx::Texture2D> albedo{};
-        std::shared_ptr<const gfx::Texture2D> normal{};
-        std::shared_ptr<const gfx::Texture2D> arm{};
+        std::shared_ptr<const gfx::Texture2D> albedo  {};
+        std::shared_ptr<const gfx::Texture2D> normal  {};
+        std::shared_ptr<const gfx::Texture2D> arm     {};
         std::shared_ptr<const gfx::Texture2D> emissive{};
     };
 }
