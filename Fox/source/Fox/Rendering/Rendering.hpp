a7b2dcae78a088beb04490f98626952b493e14cf
Jari Wilms <jariwilms@outlook.com>
2025-06-28 21:36:53 +0200

    name change

--- Fox/source/Fox/Rendering/Rendering.hpp
+++ Fox/source/Fox/Rendering/Rendering.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <fox/rendering/aabb/aabb.hpp>
+#include <fox/rendering/bounding_volume/aabb.hpp>
 #include <fox/rendering/buffer/buffer.hpp>
 #include <fox/rendering/buffer/vertexarray.hpp>
 #include <fox/rendering/framebuffer/framebuffer.hpp>
