4786fc6ce8e5c6869dbaff53152f3f8d9c2274ef
Jari Wilms <jariwilms@outlook.com>
2025-06-28 16:07:28 +0200

    cleanup

--- Fox/source/Fox/Random/Random.hpp
+++ Fox/source/Fox/Random/Random.hpp
@@ -6,13 +6,16 @@
 #include <fox/core/types/common/common.hpp>
 #include <fox/core/types/fundamental/fundamental.hpp>
 
-namespace fox::random::api
-{
-    inline auto device = std::random_device{};
-    inline auto engine = std::mt19937{ device() };
-}
 namespace fox::random
 {
+    namespace api
+    {
+        inline auto device = std::random_device{};
+        inline auto engine = std::mt19937{ device() };
+    }
+
+
+
     static void seed(fox::uint32_t seed)
     {
         api::engine.seed(seed);
@@ -21,7 +24,8 @@ namespace fox::random
     template<typename T>
     static auto next(fox::interval_t<T> interval = fox::interval_t<T>{ std::numeric_limits<T>::min(), std::numeric_limits<T>::max() })
     {
-        return distribution::uniform(interval);
+        if constexpr (std::is_integral_v      <T>) return std::uniform_int_distribution <T>{ interval.min, interval.max }(api::engine);
+        if constexpr (std::is_floating_point_v<T>) return std::uniform_real_distribution<T>{ interval.min, interval.max }(api::engine);
     }
 
     template<typename T>
@@ -30,11 +34,11 @@ namespace fox::random
         std::ranges::shuffle(data, api::engine);
     }
     template<typename T>
-    static auto choice(std::span<const T> data) -> T&
+    static auto choice (std::span<const T> data) -> T&
     {
-        if (data.empty()) throw std::invalid_argument{ "Data is empty!" };
-
-        return data[next({}, data.size() - 1u)];
+        if (data.empty()) throw std::range_error{ "Data is empty!" };
+        
+        return data[next(1u, data.size() - 1u)];
     }
 
 
@@ -44,58 +48,28 @@ namespace fox::random
         template<typename T> requires (std::is_arithmetic_v<T>)
         static auto normal   (fox::interval_t<T> interval = fox::interval_t<T>{ std::numeric_limits<T>::min(), std::numeric_limits<T>::max() })
         {
-            auto distribution = std::normal_distribution<T>
-            {
-                interval.min, 
-                interval.max, 
-            };
-
-            return distribution(api::engine);
+            return std::normal_distribution<T>{ interval.min, interval.max }(api::engine);
         }
         template<typename T> requires (std::is_arithmetic_v<T>)
         static auto uniform  (fox::interval_t<T> interval = fox::interval_t<T>{ std::numeric_limits<T>::min(), std::numeric_limits<T>::max() })
         {
-            if constexpr (std::is_integral_v      <T>)
-            {
-                auto distribution = std::uniform_int_distribution<T>
-                { 
-                    interval.min, 
-                    interval.max, 
-                };
-
-                return distribution(api::engine);
-            }
-            if constexpr (std::is_floating_point_v<T>)
-            {
-                auto distribution = std::uniform_real_distribution<T>
-                { 
-                    interval.min, 
-                    interval.max, 
-                };
-
-                return distribution(api::engine);
-            }
+            if constexpr (std::is_integral_v      <T>) return std::uniform_int_distribution <T>{ interval.min, interval.max }(api::engine);
+            if constexpr (std::is_floating_point_v<T>) return std::uniform_real_distribution<T>{ interval.min, interval.max }(api::engine);
         }
         template<typename T> requires (std::is_integral_v<T> and std::is_unsigned_v<T>)
         static auto binomial (fox::count_t trials = fox::count_t{ 1u }, fox::float64_t probability = fox::float64_t{ 0.5 })
         {
-            auto distribution = std::binomial_distribution<T>{ trials, probability };
-
-            return distribution(api::engine);
+            return std::binomial_distribution<T>{ trials, probability }(api::engine);
         } 
         template<typename T> requires (std::is_integral_v<T> and std::is_unsigned_v<T>)
         static auto bernoulli(fox::float64_t probability = fox::float64_t{ 0.5 })
         {
-            auto distribution = std::bernoulli_distribution{ probability };
-
-            return distribution(api::engine);
+            return std::bernoulli_distribution{ probability }(api::engine);
         }
         template<typename T> requires (std::is_integral_v<T> and std::is_unsigned_v<T>)
         static auto poisson  (fox::float64_t mean = fox::float64_t{ 1.0 })
         {
-            auto distribution = std::poisson_distribution{ mean };
-
-            return distribution(api::engine);
+            return std::poisson_distribution{ mean }(api::engine);
         }
     }
 }
