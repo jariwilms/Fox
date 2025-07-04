506580d4841dccb8514d1dda2b7d8e50dc7ced3d
Jari Wilms <jariwilms@outlook.com>
2025-07-04 16:34:18 +0200

    hardening

--- fox/source/fox/math/texture/texture.hpp
+++ fox/source/fox/math/texture/texture.hpp
@@ -3,10 +3,12 @@
 #include <glm/gtx/texture.hpp>
 
 #include <fox/core/types/fundamental/fundamental.hpp>
+#include <fox/core/types/math/linear_algebra/vector.hpp>
 
 namespace fox::math
 {
-    static constexpr auto mipmap_levels(const auto& vector)
+    template<std::integral T, fox::uint32_t N>
+    static constexpr auto mipmap_levels(const fox::Vector<T, N>& vector) -> fox::uint32_t
     {
         return static_cast<fox::uint32_t>(glm::levels(vector));
     }
