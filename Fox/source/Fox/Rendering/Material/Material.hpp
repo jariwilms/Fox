da0d1d74cac8193ad3ed52d45ab170f338547781
Jari Wilms <jariwilms@outlook.com>
2025-07-08 23:43:30 +0200

    formatting

--- Fox/source/Fox/Rendering/Material/Material.hpp
+++ Fox/source/Fox/Rendering/Material/Material.hpp
@@ -2,22 +2,23 @@
 
 #include <stdafx.hpp>
 
+#include <fox/rendering/shader/shader.hpp>
 #include <fox/rendering/texture/texture.hpp>
 
 namespace fox::gfx
 {
     struct Material
     {
-    public:
-        std::string name{ "Material" };
+        std::string                           name;
+        fox::Vector4f                         color          { vector::one };
+        fox::float32_t                        roughnessFactor{ 0.5f };
+        fox::float32_t                        metallicFactor { 0.0f };
 
-        fox::Vector4f  color          { 1.0f };
-        fox::float32_t roughnessFactor{ 0.5f };
-        fox::float32_t metallicFactor { 0.0f };
+        std::shared_ptr<const gfx::Texture2D> albedo;
+        std::shared_ptr<const gfx::Texture2D> normal;
+        std::shared_ptr<const gfx::Texture2D> arm;
+        std::shared_ptr<const gfx::Texture2D> emissive;
 
-        std::shared_ptr<const gfx::Texture2D> albedo  {};
-        std::shared_ptr<const gfx::Texture2D> normal  {};
-        std::shared_ptr<const gfx::Texture2D> arm     {};
-        std::shared_ptr<const gfx::Texture2D> emissive{};
+        std::shared_ptr<const gfx::Shader>    shader;
     };
 }
