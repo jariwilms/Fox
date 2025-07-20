c83c0208c00ab653d98a832da68851b618fe9dc4
Jari <jariwilms@outlook.com>
2025-07-20 11:50:28 +0200

    project name changes compile -> compile_shaders

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,5 @@
-workspace "Fox Engine"
-	startproject "RUN"
+workspace "fox-engine"
+	startproject "run"
 	architecture "x64"
 	
 	configurations { 
@@ -7,15 +7,15 @@ workspace "Fox Engine"
 		"Release", 
 	}
 	includedir = {
-		["ASSIMP"  ] = "vendor/assimp/include", 
-		["ENTT"    ] = "vendor/entt/single_include", 
-		["GLAD"    ] = "vendor/glad/include", 
-		["GLFW"    ] = "vendor/glfw/include", 
-		["GLM"     ] = "vendor/glm/include", 
-		["JOLT"    ] = "vendor/jolt/include", 
-		["MIMALLOC"] = "vendor/mimalloc/include", 
-		["NLOHMANN"] = "vendor/nlohmann/include", 
-		["STB"     ] = "vendor/stb/include", 
+		["assimp"  ] = "vendor/assimp/include", 
+		["entt"    ] = "vendor/entt/single_include", 
+		["glad"    ] = "vendor/glad/include", 
+		["glfw"    ] = "vendor/glfw/include", 
+		["glm"     ] = "vendor/glm/include", 
+		["jolt"    ] = "vendor/jolt/include", 
+		["mimalloc"] = "vendor/mimalloc/include", 
+		["nlohmann"] = "vendor/nlohmann/include", 
+		["stb"     ] = "vendor/stb/include", 
 	}
 
 	filter "system:windows"
@@ -45,7 +45,7 @@ workspace "Fox Engine"
 		local m     = premake.vstudio.vc2010
 		local calls = base(prj)
 		
-		if premake.project.iscpp(prj) and string.find(prj.filename, "FOX") then
+		if premake.project.iscpp(prj) and string.find(prj.filename, "fox") then
 			table.insertafter(calls, premake.xmlDeclaration,  function()
 				premake.w('<ModuleDependenciesFile>$(IntDir)\\%%(RelativeDir)</ModuleDependenciesFile>')
 				premake.w('<ModuleOutputFile>$(IntDir)\\%%(RelativeDir)</ModuleOutputFile>')
@@ -55,6 +55,98 @@ workspace "Fox Engine"
 
 		return calls
 	end)
+
+
+
+group "Application"
+	project "fox"
+		location         "fox"
+		language         "C++"
+		cppdialect       "C++23"
+		kind             "StaticLib"
+		staticruntime    "On"
+		enablemodules    "On"
+		buildstlmodules  "On"
+		warnings         "Extra"
+		externalwarnings "Off"
+		
+		defines {
+			'FOX_ENGINE', 
+			"FOX_MALLOC", 
+			'FOX_ASSET_DIR=R"($(ProjectDir)assets\\.)"', 
+			
+			"GLFW_INCLUDE_NONE", 
+		}
+		includedirs {
+			"fox/source", 
+			
+			"%{includedir.assimp}", 
+			"%{includedir.entt}", 
+			"%{includedir.glad}", 
+			"%{includedir.glfw}", 
+			"%{includedir.glm}", 
+			"%{includedir.jolt}", 
+			"%{includedir.mimalloc}", 
+			"%{includedir.nlohmann}", 
+			"%{includedir.stb}", 
+		}
+		files {
+			"fox/source/**.hpp", 
+			"fox/source/**.cpp", 
+			"fox/source/**.ixx", 
+		}
+		links {
+			"assimp", 
+			"entt", 
+			"glad", 
+			"glfw", 
+			"glm", 
+			"jolt", 
+			"mimalloc", 
+			"nlohmann", 
+			"stb", 
+			
+			"opengl32.lib", 
+		}
+		
+		filter "system:windows"
+			systemversion "latest"
+			
+			defines {
+				"NOMINMAX", 
+				"FOX_PLATFORM_WINDOWS", 
+			}
+		
+		filter "configurations:Debug"
+			defines   "FOX_DEBUG"
+			
+		filter "configurations:Release"
+			defines   "FOX_RELEASE"
+
+
+
+	project "run"
+		location         "run"
+		language         "C++"
+		cppdialect       "C++23"
+		kind             "ConsoleApp"
+		staticruntime    "On"
+		enablemodules    "On"
+		buildstlmodules  "On"
+		warnings         "Extra"
+		externalwarnings "Off"
+		
+		includedirs {
+			"fox/source", 
+		}
+		files {
+			"run/source/**.hpp", 
+			"run/source/**.cpp", 
+			"run/source/**.ixx", 
+		}
+		links {
+			"fox", 
+		}
 
 
 
@@ -68,105 +160,4 @@ group "Dependencies"
 	include "vendor/mimalloc"
 	include "vendor/nlohmann"
 	include "vendor/stb"
-group ""
-
-group "Application"
-project "FOX"
-	location         "fox"
-	language         "C++"
-	cppdialect       "C++23"
-	kind             "StaticLib"
-	staticruntime    "On"
-	enablemodules    "On"
-	buildstlmodules  "On"
-	warnings         "Extra"
-	externalwarnings "Off"
-	
-	defines {
-		'FOX_ENGINE', 
-		"FOX_MALLOC", 
-		'FOX_ASSET_DIR=R"($(ProjectDir)assets\\.)"', 
-		
-		"GLFW_INCLUDE_NONE", 
-	}
-	includedirs {
-		"fox/source", 
-		
-		"%{includedir.ASSIMP}", 
-		"%{includedir.ENTT}", 
-		"%{includedir.GLAD}", 
-		"%{includedir.GLFW}", 
-		"%{includedir.GLM}", 
-		"%{includedir.JOLT}", 
-		"%{includedir.MIMALLOC}", 
-		"%{includedir.NLOHMANN}", 
-		"%{includedir.STB}", 
-	}
-	files {
-		"fox/source/**.hpp", 
-		"fox/source/**.cpp", 
-		"fox/source/**.ixx", 
-	}
-	links {
-		"ASSIMP", 
-		"ENTT", 
-		"GLAD", 
-		"GLFW", 
-		"GLM", 
-		"JOLT", 
-		"MIMALLOC", 
-		"NLOHMANN", 
-		"STB", 
-		
-		"opengl32.lib", 
-	}
-	
-	filter "system:windows"
-		systemversion "latest"
-		
-		defines {
-			"NOMINMAX", 
-			"FOX_PLATFORM_WINDOWS", 
-		}
-	
-	filter "configurations:Debug"
-		defines   "FOX_DEBUG"
-		
-	filter "configurations:Release"
-		defines   "FOX_RELEASE"
-
-
-
-project "RUN"
-	location         "run"
-	language         "C++"
-	cppdialect       "C++23"
-	kind             "ConsoleApp"
-	staticruntime    "On"
-	enablemodules    "On"
-	buildstlmodules  "On"
-	warnings         "Extra"
-	externalwarnings "Off"
-	
-	includedirs {
-		"fox/source", 
-	}
-	files {
-		"run/source/**.hpp", 
-		"run/source/**.cpp", 
-		"run/source/**.ixx", 
-	}
-	links {
-		"FOX", 
-		
-		"ASSIMP", 
-		"ENTT", 
-		"GLAD", 
-		"GLFW", 
-		"GLM", 
-		"JOLT", 
-		"MIMALLOC", 
-		"NLOHMANN", 
-		"STB", 
-	}
 group ""
